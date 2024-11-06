c9f1572340ae7bd433ccaa7812b0f3f250f168b6
Leon Tang <130707295+tangleon8@users.noreply.github.com>
2024-11-06 17:59:09 -0500

    Create cars.js

---
+++ cars.js
@@ -0,0 +1,7 @@
+let cars = ["Honda", "BMW", "Toyota"];
+
+document.write("<ul>");
+for (let i = 0; i < cars.length; i++) {
+    document.write("<li>" + cars[i] + "</li>"); 
+}
+document.write("</ul>");
